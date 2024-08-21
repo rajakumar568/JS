4fb57ac23437cfca91d920e218cd22770db12c1a
Raja Kumar <rajakumarsah568@gmail.com>
2024-08-21 13:26:27 +0530

    20-08-2024-2

--- 32_ColorChangeProject.js
+++ 32_ColorChangeProject.js
@@ -1,27 +1,58 @@
+// const buttons=document.querySelectorAll('.box')
+// // console.log(buttons);
+// const body=document.querySelector('body')
+
+// buttons.forEach(function(button){
+//     // console.log(button);
+//     button.addEventListener('click',function(e){
+//         // console.log(e);
+//         // console.log(e.target);  
+//         if (e.target.id==='grey') {
+//             body.style.backgroundColor=e.target.id
+//         }
+//         if (e.target.id==='red') {
+//             body.style.backgroundColor=e.target.id
+//         }
+//         if (e.target.id==='green') {
+//             body.style.backgroundColor=e.target.id
+//         }
+//         if (e.target.id==='blue') {
+//             body.style.backgroundColor=e.target.id
+//         }
+//         if (e.target.id==='aqua') {
+//             body.style.backgroundColor=e.target.id
+//         }
+//     })
+    
+// })
+
+
 const buttons=document.querySelectorAll('.box')
-// console.log(buttons);
+const text=document.querySelector('.heading')
 const body=document.querySelector('body')
 
 buttons.forEach(function(button){
-    // console.log(button);
     button.addEventListener('click',function(e){
-        // console.log(e);
-        // console.log(e.target);  
-        if (e.target.id==='grey') {
+        if (e.target.id=='grey') {
             body.style.backgroundColor=e.target.id
         }
-        if (e.target.id==='red') {
+        if (e.target.id=='red') {
             body.style.backgroundColor=e.target.id
         }
-        if (e.target.id==='green') {
+        if (e.target.id=='blue') {
             body.style.backgroundColor=e.target.id
         }
-        if (e.target.id==='blue') {
+        if (e.target.id=='green') {
             body.style.backgroundColor=e.target.id
         }
-        if (e.target.id==='aqua') {
+        if (e.target.id=='aqua') {
             body.style.backgroundColor=e.target.id
+            text.style.color='orange'
         }
     })
-    
+
 })
+
+
+
+// Here we are doing some changes for future ref.
